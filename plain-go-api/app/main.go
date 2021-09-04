d10523e4cb35226f7b6f9d760e2ac860b764ddc2
SuguruOoki <oki.work0527@gmail.com>
2021-09-04 09:56:01 +0900

    [ADD]メモのためのAPIのルートを追加

--- plain-go-api/app/main.go
+++ plain-go-api/app/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
@@ -10,12 +9,25 @@ import (
 )
 
 func main() {
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/memos", MemoIndex)
+	router.HandleFunc("/memos/{memoId}", MemoShow)
 
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	fmt.Fprintln(w, "Welcome!")
+}
+
+func MemoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Memo Index!")
+}
+
+func MemoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	memoId := vars["memoId"]
+	fmt.Fprintln(w, "Memo show:", memoId)
 }
