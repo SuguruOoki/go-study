a7ab121767d6a4953e11c77ec23edd5e8f5f4f62
SuguruOoki <oki.work0527@gmail.com>
2021-09-12 23:18:12 +0900

    [MODIFY]諸々修正

--- plain-go-api/mysql/docker-entrypoint-initdb.d/init.sql
+++ plain-go-api/mysql/docker-entrypoint-initdb.d/init.sql
@@ -2,8 +2,6 @@ CREATE DATABASE IF NOT EXISTS `go_sample` COLLATE 'utf8mb4_general_ci';
 
 CREATE DATABASE IF NOT EXISTS `go_sample_test` COLLATE 'utf8mb4_general_ci';
 
-GRANT ALL ON `go_sample`.* TO 'localuser' @'%';
-
-GRANT ALL ON `go_sample_test`.* TO 'localuser' @'%';
-
+-- GRANT ALL ON `go_sample`.* TO 'localuser' @'%';
+-- GRANT ALL ON `go_sample_test`.* TO 'localuser' @'%';
 FLUSH PRIVILEGES;
